0f88a7f1b3f074a12453542ed2f9d0d139a7413c
agooddevil1 <60966499+agooddevil1@users.noreply.github.com>
2020-10-28 16:21:37 +0530

    navbar , navbar changer

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ recognition.continous = true;
 recognition.lang = "en-US";
 recognition.interimResults = false;
 recognition.maxAlternatives = 2;
+recognition.rate = 1.5;
 let speech = new SpeechSynthesisUtterance();
-
-const startBtn = document.querySelector("#startBtn");
+let navBar;
 const speakBtn = document.querySelector("#speakBtn");
 
+let NavItem = `<li class="nav-item">
+<a class="nav-link active" href="#">SampleLink</a>
+</li>`;
 // random greeting message returns
 function greetingMessage() {
   // messages array declared
@@ -38,6 +41,16 @@ function getCurrentDay() {
   let index = newDate.getDay();
   return days[index];
 }
+function returnNumber(numberText){
+  if (numberText === 'to') {
+    return 2
+  }
+  let arr = ['zero' , 'one','two','three','four','five','six','seven','eight','nine'];
+  let index = arr.indexOf(numberText);
+  return index
+
+
+}
 
 speakBtn.addEventListener("click", () => {
   recognition.start();
@@ -51,7 +64,8 @@ recognition.onresult = (e) => {
 
   const arr = str.split(" ");
 
-  // if we speak "hello" then we fetch a random greeting message from thr greetingMessage function
+  try {
+    // if we speak "hello" then we fetch a random greeting message from thr greetingMessage function
   if (arr.includes("hello")) {
     // set the text to the greetingMessage
     speech.text = greetingMessage();
@@ -68,24 +82,41 @@ recognition.onresult = (e) => {
     window.speechSynthesis.speak(speech);
   }
 
-  if (arr.includes("increase")) {
-    speech.text = `Font Size Increased`;
+ 
+  
+  if (arr.includes("header") && arr.includes('create')) {
+    speech.text = `Header added successfully with  ${arr[arr.length-2]} links`;
     window.speechSynthesis.speak(speech);
-    startBtn.style.fontSize = "3rem";
+    navBar = document.createElement("ul");
+    navBar.classList.add("nav");
+    navBar.classList.add("justify-content-center");
+    let navElements = returnNumber(arr[arr.length-2]);
+    for (let i = 0; i < navElements; i++) {
+      navBar.innerHTML+=NavItem;
+    }
+  document.querySelector('.container').appendChild(navBar);
   }
-  if (arr.includes("decrease")) {
-    speech.text = `Font Size Decreased`;
-    window.speechSynthesis.speak(speech);
-    startBtn.style.fontSize = "1rem";
+  // change header link text of number two to contect
+  if(arr.includes('change') && arr.includes('header') ){
+    if(arr.includes('text') ){
+      let linkNumber=returnNumber(arr[arr.length-3]);
+      navBar.children[linkNumber].children[0].innerText=arr[arr.length-1]
+
+    }
+    // change header link color of all to red
+  else if(arr.includes('colour') && arr.includes('all')){
+    for(let i =0; i<navBar.children.length; i++){
+      navBar.children[i].children[0].style.color=arr[arr.length-1]
+    }
   }
-  if (arr.includes("red")) {
-    speech.text = `Color changed to Red`;
-    window.speechSynthesis.speak(speech);
-    startBtn.style.color = "red";
   }
-  if (arr.includes("blue")) {
-    speech.text = `Color changed to Blue`;
-    window.speechSynthesis.speak(speech);
-    startBtn.style.color = "blue";
-  }
+  // else {
+  //   speech.text='sorry i dont understand';
+  // window.speechSynthesis.speak(speech)
+  // }
+} catch (error) {
+  speech.text='sorry i dont understand';
+  window.speechSynthesis.speak(speech)
+  console.log('catch is executed')
+}
 };
