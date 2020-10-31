d89e595f41a02ce15db107b03c18b40d686f8ecc
Praharsh Singh <praharsh535@gmail.com>
2020-10-31 11:36:29 +0530

    previous code deleted and navbar code added

--- script.js
+++ script.js
@@ -1,67 +1,122 @@
-const recognition = new webkitSpeechRecognition();
+var recognition = new webkitSpeechRecognition();
+
 recognition.continous = true;
 recognition.lang = "en-US";
+
 recognition.interimResults = false;
 recognition.maxAlternatives = 2;
-recognition.rate = 1.5;
+recognition.rate = 1.1;
+
 let speech = new SpeechSynthesisUtterance();
-let NavItem = `<li class="nav-item">
-<a class="nav-link active" href="#">SampleLink</a>
+let allVoices;
+window.speechSynthesis.onvoiceschanged = function () {
+  allVoices = window.speechSynthesis.getVoices();
+
+  // 0 -> jarvis voice
+  speech.voice = allVoices[0];
+};
+let speechResult;
+const speakBtn = document.querySelector("#speakBtn");
+speakBtn.addEventListener("click", () => {
+  recognition.start();
+});
+
+function botSpeak(speechText) {
+  speech.text = speechText;
+  window.speechSynthesis.speak(speech);
+}
+
+let gender = "Sir";
+// let gender = "Mam";
+
+let Navbar, logo;
+
+let emptyNav = `<nav class="navbar navbar-light bg-light"></nav>`;
+let navLink = `<li class="nav-item">
+<a class="nav-link" href="#">Link</a>
 </li>`;
-// let NavText = `<ul class="nav">
-// <li class="nav-item">
-//   <a class="nav-link active" href="#">SampleLink</a>
-// </li>
-// <li class="nav-item">
-//   <a class="nav-link" href="#">SampleLink</a>
-// </li>
-// <li class="nav-item">
-//   <a class="nav-link" href="#">SampleLink</a>
-// </li>
+
+let mainSection = document.querySelector(".mainSection");
+
+// random greeting message returns
+function greetingMessage() {
+  // messages array declared
+  let messages = [
+    `Hello ${gender}, How may i help u ? `,
+    `Good morning ${gender}, I hope you are having a wonderful day`,
+    `Good morning ${gender}, I hope you enjoyed your weekend`,
+    `Good morning ${gender}, I hope you are having a great week`,
+    `Good morning ${gender}, I hope you are doing well`,
+    `Good morning ${gender}, I hope you have coffee already`,
+    `Good morning ${gender}, I hope you have your attendance more than 75%`,
+  ];
+  // random index generated of messages array
+  let index = Math.floor(Math.random() * messages.length);
+  // random message from the messages array return
+  return messages[index];
+}
 
 // </ul>`;
 // const startBtn = document.querySelector("#startBtn");
 const speakBtn = document.querySelector("#speakBtn");
 
-function addNavbar() {
-  // let NavBar = document.createElement("Navbar");
-  // NavBar.innerHTML = NavText;
-  // document.querySelector(".container").appendChild(NavBar);
-  // NavBar.firstElementChild.classList.add("justify-content-center");
+function check(text) {
+  return speechResult.includes(text);
 }
-
-function greeting() {
-  return "HELLO FROM GOKU";
-}
-
-const startBtn = document.querySelector("#startBtn");
-const speakBtn = document.querySelector("#speakBtn");
-
-speakBtn.addEventListener("click", () => {
-  recognition.start();
-});
 
 recognition.onresult = (e) => {
   const str = e.results[0][0]["transcript"];
   console.log(str);
   speech.text = str;
-  //   window.speechSynthesis.speak(speech);
-
-  const arr = str.split(" ");
-
-  if (arr.includes("hello")) {
-    speech.text = greeting();
-    window.speechSynthesis.speak(speech);
+  speechResult = str.split(" ");
+  // STARTING COMMANDS
+  if (check("hello")) {
+    botSpeak(greetingMessage());
   }
-  if (arr.includes("header") && arr.includes("create")) {
-    speech.text = "header created successfully";
-    window.speechSynthesis.speak(speech);
-    let navBar = document.createElement("ul");
-    navBar.classList.add("nav");
-    navBar.classList.add("justify-content-center");
-    let navElements = parseInt(arr[arr.length - 2]);
-    for (let i = 0; i < navElements; i++) {
-      navBar.appendChild(NavItem);
+  if (check("current") && check("day")) {
+    botSpeak(`Today is ${getCurrentDay()}`);
+  }
+
+  //HEADER COMMANDS
+
+  // CREATE HEADER / NAVBAR
+  if (check("header") || check("navbar")) {
+    if (check("create")) {
+      Navbar = document.createElement("nav");
+      Navbar.classList.add("navbar");
+
+      mainSection.appendChild(Navbar);
+      let navName = check("header") ? "header" : "navbar";
+      botSpeak(`${navName} created Successfully`);
+    }
+  }
+  //  ADD BRAND LOGO
+  if (check("add")) {
+    if (check("logo") && check("brand")) {
+      logo = document.createElement("a");
+      logo.classList.add("navbar-brand");
+      let logoImg = document.createElement("img");
+      logoImg.src = "../images/logo.png";
+      logoImg.setAttribute("height", "80px");
+      // logoImg.setAttribute("width", "50px");
+      logo.appendChild(logoImg);
+      Navbar.appendChild(logo);
+      botSpeak("Logo added Successfully");
+    }
+    // ADD LOGO TEXT
+    if (check("logo") && check("text")) {
+      var logoText = speechResult.slice(4, speechResult.length);
+      if (logoText.length > 2 || logoText == 0) {
+        botSpeak(
+          "Sorry sir.Please provide logo text of minimum 1 word or maximum 2 words"
+        );
+      } else {
+        for (let i = 0; i < logoText.length; i++) {
+          logo.innerHTML += logoText[i];
+        }
+        botSpeak("logo text added successfully");
+      }
+      console.log(logoText);
     }
   }
 };
